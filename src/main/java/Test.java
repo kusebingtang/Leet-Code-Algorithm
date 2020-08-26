6a41af6cc5d70b837235b39c850e735037285b95
jbw <jb@98game.cn>
2020-08-26 09:23:33 +0800

    add 训练营二期代码

---
+++ src/main/java/Test.java
@@ -0,0 +1,11 @@
+/**
+ * @author JiangBin
+ * @create 2020-08-25 10:20
+ */
+public class Test {
+
+    public static void main(String[] args) {
+//        System.out.println(Boolean.);
+        int a = 10;
+    }
+}
